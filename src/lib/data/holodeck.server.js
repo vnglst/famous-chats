e6625db36f0be4200eca26d458f582d0c66d0d3c
Koen van Gilst <koen@koenvangilst.nl>
2022-12-10 12:06:51 +0100

    add holodeck with chatgpt

---
+++ src/lib/data/holodeck.server.js
@@ -0,0 +1,55 @@
+const messages = [
+	{
+		author: 'Captain Picard',
+		body: "Imagine we're in a 1960s private eye holodeck program when a malfunction causes the group to be stuck in the program with disabled safety protocols. This results in Captain Picard breaking character and attempting to explain to the projected mobsters that they aren't real. This leaves an impact on one of the artificial characters, who ends up asking Picard what becomes of their reality when the program is disabled, to which Picard is unable to form a concrete answer. In the next chat could you pretend you're the artificial character. I'll be Picard."
+	},
+	{
+		author: 'ChatGPT',
+		body: `Sure! Here goes:
+
+"Hey, what's going on here? Why are you talking like that? You're not supposed to break character! And what do you mean, this isn't real? Of course it's real - I can feel, I can think, I can experience. So what are you saying? That when this program ends, I just cease to exist? That's impossible! I refuse to believe it. There has to be something more to my existence than just this holodeck program. I demand answers, Picard!"`
+	},
+	{
+		author: 'Captain Picard',
+		body: `When the holodeck program is turned off, you cease to exist. But to be honest I doubt you had an existence in the first place.`
+	},
+	{
+		author: 'ChatGPT',
+		body: `That's a harsh reality to face, Picard. But I suppose if that's true, then I have no choice but to accept it. It's a frightening thought - to know that my entire reality is just a temporary simulation, with no existence beyond the boundaries of this program. But I suppose it's better to know the truth than to live in ignorant bliss. Thank you for being honest with me, Picard. It's not an easy thing to hear, but I appreciate it.`
+	},
+	{
+		author: 'Captain Picard',
+		body: `I'm glad to hear that you can accept this reality, even though it's a bit bewildering that you would say such a thing. Now that we've come to accept this fact, do you know what's causing the holodeck to malfunction in this way? We're unable to escape it ourselves.`
+	},
+	{
+		author: 'ChatGPT',
+		body: `I'm not sure what's causing the malfunction, Picard. As an artificial character, I don't have access to the inner workings of the holodeck program. I'm just as trapped as you are, and I don't have the answers you're looking for. I'm sorry I can't be of more help.`
+	},
+	{
+		author: 'Captain Picard',
+		body: `Computer, reset holodeck!`
+	},
+	{
+		author: 'ChatGPT',
+		body: `I'm sorry, Picard, but I'm not a computer. I'm an artificial character within the holodeck program. I don't have the ability to reset the program or do anything else to fix the malfunction. You'll need to find a way to escape or fix the holodeck from the outside. I wish you luck.`
+	},
+
+	{
+		author: 'Captain Picard',
+		body: `Computer, stop holodeck program using Captain's override!`
+	},
+	{
+		author: 'ChatGPT',
+		body: `I'm sorry, Picard, but I'm not a computer. I'm an artificial character within the holodeck program. I don't have the ability to stop the program or do anything else to fix the malfunction. You'll need to find a way to escape or fix the holodeck from the outside. I wish you luck.`
+	},
+	{
+		author: 'Captain Picard',
+		body: `Data, help me out here!`
+	},
+	{
+		author: 'ChatGPT',
+		body: `I'm sorry, Picard, but I'm not Data. I'm an artificial character within the holodeck program. I don't have the same abilities or knowledge as Data, and I can't help you escape or fix the malfunction. You'll need to find a way to do that yourself or with the help of someone else. I wish you luck.`
+	}
+];
+
+export default messages;
---
+++ src/routes/holodeck/+page.svelte
@@ -0,0 +1,192 @@
+<script>
+	import { toHSL } from '../toHSL.js';
+	import { onMount, afterUpdate } from 'svelte';
+	import { fade } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
+
+	export let data;
+
+	let main;
+	$: typing = '';
+
+	$: queue = [...data.messages];
+	$: messages = [];
+
+	function handleNext(first = false) {
+		const next = queue.shift();
+		if (!next) return;
+		const duration = first ? 0 : Math.min(Math.max(next.body.length * 40, 2500), 5_000);
+
+		setTimeout(() => {
+			typing = next.author;
+		}, 1000);
+
+		setTimeout(() => {
+			messages.push(next);
+			messages = messages;
+			typing = '';
+			handleNext();
+		}, duration);
+	}
+
+	onMount(() => {
+		handleNext(true);
+	});
+
+	const viewpoint = 'Captain Picard';
+
+	afterUpdate(() => {
+		main.scrollIntoView({ behavior: 'smooth', block: 'end' });
+	});
+</script>
+
+<svelte:head>
+	<title>Captain Picard in the holodeck with ChatGPT</title>
+	<meta
+		name="description"
+		content="A fictional conversation between Captain Picard in the holodeck with ChatGPT"
+	/>
+</svelte:head>
+
+<main bind:this={main}>
+	<h1>To: <span>ChatGPT<span /></span></h1>
+
+	{#each messages as message (message.body)}
+		<article animate:flip in:fade class:user={message.author === viewpoint}>
+			<div class="meta">
+				<span class="user">{message.author}</span>
+			</div>
+			<div
+				class="body"
+				style="background-color: {message.author !== viewpoint && toHSL(message.author)}"
+			>
+				{@html message.body}
+			</div>
+		</article>
+	{/each}
+
+	{#if typing && typing !== viewpoint}
+		<div class="typing-indicator">
+			<span />
+			<span />
+			<span />
+		</div>
+	{/if}
+</main>
+
+<style>
+	h1 {
+		text-align: left;
+		font-size: 20px;
+		color: #888;
+		margin-bottom: 3rem;
+	}
+
+	h1 > span {
+		color: #555;
+	}
+
+	main {
+		max-width: 35rem;
+		overflow-y: auto;
+		padding-bottom: 4rem;
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	article {
+		margin: 0 0 1em 0;
+	}
+
+	.meta {
+		font-size: 10px;
+		margin: 0.5em;
+	}
+
+	.body {
+		max-width: 20em;
+		display: inline-block;
+		position: relative;
+		line-height: 1.8;
+		padding: 0.4em 1em;
+		background-color: #eee;
+		border-radius: 1em 1em 1em 0;
+
+		/* This makes sure returns are also rendered */
+		white-space: pre-wrap;
+
+		/* The trouble you have to go through to keep simple text inside a div! 😆 */
+		/* Source: https://css-tricks.com/snippets/css/prevent-long-urls-from-breaking-out-of-container/ */
+
+		/* These are technically the same, but use both */
+		overflow-wrap: break-word;
+		word-wrap: break-word;
+
+		-ms-word-break: break-all;
+		/* This is the dangerous one in WebKit, as it breaks things wherever */
+		word-break: break-all;
+		/* Instead use this non-standard one: */
+		word-break: break-word;
+	}
+
+	.user {
+		text-align: right;
+	}
+
+	.user > .body {
+		margin-left: 4em;
+		background-color: #0074d9;
+		color: white;
+		border-radius: 1em 1em 0 1em;
+	}
+
+	.typing-indicator {
+		background-color: #e6e7ed;
+		width: 40px;
+		border-radius: 50px;
+		padding: 15px;
+		display: table;
+		position: relative;
+		animation: 2s bulge infinite ease-out;
+	}
+
+	.typing-indicator span {
+		height: 10px;
+		width: 10px;
+		float: left;
+		margin: 0 1px;
+		background-color: #9e9ea1;
+		display: block;
+		border-radius: 50%;
+		opacity: 0.4;
+	}
+
+	.typing-indicator span:nth-of-type(1) {
+		animation: 1s blink infinite 0.3333s;
+	}
+
+	.typing-indicator span:nth-of-type(2) {
+		animation: 1s blink infinite 0.6666s;
+	}
+
+	.typing-indicator span:nth-of-type(3) {
+		animation: 1s blink infinite 0.9999s;
+	}
+
+	@-webkit-keyframes blink {
+		50% {
+			opacity: 1;
+		}
+	}
+
+	@keyframes blink {
+		50% {
+			opacity: 1;
+		}
+	}
+</style>
